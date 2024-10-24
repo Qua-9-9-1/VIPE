b9c5f10ab6d8692ed00d50d1b3d446826bd1fada
Quentin Duval <quentin1.duval@epitech.eu>
2024-10-24 17:28:09 +0200

    [ADD] complete toolkit

--- include/Toolkit.hpp
+++ include/Toolkit.hpp
@@ -1,39 +1,43 @@
+#pragma once
+
 #include <gtkmm.h>
+#include <vector>
 #include <iostream>
 
-enum tools {
-    pen,
-    pencil,
-    eraser,
-    brush,
-    spray,
-    paint_bucket,
-    text,
-    line,
-    rectangle,
-    circle,
-    lasso,
-    magic_wand,
-    shape,
-    gradient
-};
+#include "FloatingPanel.hpp"
 
 namespace vipe {
-    class Toolkit {
-        public:
-            Toolkit();
-            ~Toolkit();
-            static void load_css(const std::string& path) {
-                auto css_provider = Gtk::CssProvider::create();
-                css_provider->load_from_path(path);
 
-                auto screen = Gdk::Screen::get_default();
-                auto style_context = Gtk::StyleContext::create();
-                style_context->add_provider_for_screen(screen, css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
-            }
-        private:
-            tools _current_tool;
-            bool  _display_popup;
-            
+    enum Tool {
+        pencil,
+        brush,
+        eraser,
+        spray,
+        bucket,
+        text,
+        line,
+        rectangle,
+        circle,
+        lasso,
+        magic_wand,
+        shape,
+        gradient
     };
+
+    class Toolkit {
+    public:
+        Toolkit();
+        ~Toolkit();
+        int get_current_tool() { return _current_tool; }
+        void build_tool_grid();
+        void on_tool_selected(Tool tool);
+        FloatingPanel &get_tool_panel() { return _tool_panel; }
+
+    private:
+        Tool _current_tool;
+        Gtk::Grid _icon_grid;
+        std::vector<Gtk::Button*> _tool_buttons;
+        FloatingPanel _tool_panel;
+    };
+
 }
