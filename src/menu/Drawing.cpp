bd9e6d248bf931f438bef96aab2132c4c31373f6
Quentin Duval <quentin1.duval@epitech.eu>
2024-11-22 17:04:46 +0100

    [FIX] new drawing methods + menu options variable name

--- src/menu/Drawing.cpp
+++ src/menu/Drawing.cpp
@@ -20,20 +20,6 @@ void Canva::cursor_draw(int x1, int y1, int size) {
     set_point_pos(x1, y1);
 }
 
-void Canva::cursor_erase(int x1, int y1, int size) {
-    auto& layer = _layers[_selected_layer].image;
-
-    apply_canva_drawing_factors(x1, y1);
-    if (layer.empty()) {
-        return;
-    }
-    if (layer.channels() == 4 && (_prev_x != -1 && _prev_y != -1)) {
-        cv::line(layer, cv::Point(_prev_x, _prev_y), cv::Point(x1, y1), cv::Scalar(0, 0, 0, 0),
-                 size);
-    }
-    set_point_pos(x1, y1);
-}
-
 void Canva::cursor_square(int x1, int y1, int size) {
     auto& layer = _layers[_selected_layer].image;
 
@@ -44,12 +30,10 @@ void Canva::cursor_square(int x1, int y1, int size) {
     if (_prev_x != -1 && _prev_y != -1) {
         float dist  = std::sqrt(std::pow(x1 - _prev_x, 2) + std::pow(y1 - _prev_y, 2));
         int   steps = std::max(1, static_cast<int>(dist / (size / 2)));
-
         for (int i = 0; i <= steps; ++i) {
             float alpha = static_cast<float>(i) / steps;
             int   xi    = _prev_x + alpha * (x1 - _prev_x);
             int   yi    = _prev_y + alpha * (y1 - _prev_y);
-
             cv::rectangle(layer, cv::Point(xi - size / 2, yi - size / 2),
                           cv::Point(xi + size / 2, yi + size / 2), _color, cv::FILLED);
         }
@@ -64,20 +48,16 @@ void Canva::cursor_triangle(int x1, int y1, int size) {
     apply_canva_drawing_factors(x1, y1);
     if (layer.empty())
         return;
-
     if (_prev_x != -1 && _prev_y != -1) {
         float dist  = std::sqrt(std::pow(x1 - _prev_x, 2) + std::pow(y1 - _prev_y, 2));
         int   steps = std::max(1, static_cast<int>(dist / (size / 2)));
-
         for (int i = 0; i <= steps; ++i) {
-            float alpha = static_cast<float>(i) / steps;
-            int   xi    = _prev_x + alpha * (x1 - _prev_x);
-            int   yi    = _prev_y + alpha * (y1 - _prev_y);
-
+            float                  alpha  = static_cast<float>(i) / steps;
+            int                    xi     = _prev_x + alpha * (x1 - _prev_x);
+            int                    yi     = _prev_y + alpha * (y1 - _prev_y);
             std::vector<cv::Point> points = {cv::Point(xi, yi - size / 2),
                                              cv::Point(xi - size / 2, yi + size / 2),
                                              cv::Point(xi + size / 2, yi + size / 2)};
-
             cv::fillConvexPoly(layer, points, _color);
         }
     }
@@ -122,15 +102,14 @@ void Canva::cursor_spray(int x1, int y1, int size) {
     set_point_pos(x1, y1);
 }
 
-void Canva::color_fill(bool erase) {
-    auto& layer      = _layers[_selected_layer].image;
-    auto  fill_color = erase ? cv::Scalar(0, 0, 0, 0) : _color;
-
+void Canva::color_fill() {
+    auto& layer = _layers[_selected_layer].image;
     if (layer.empty())
         return;
 
     if (layer.channels() == 4 && (_prev_x != -1 && _prev_y != -1)) {
-        cv::floodFill(layer, cv::Point(_prev_x, _prev_y), fill_color);
+        cv::floodFill(layer, cv::Point(_prev_x, _prev_y), _color, 0, cv::Scalar(0, 0, 0, 0),
+                      cv::Scalar(0, 0, 0, 0), cv::FLOODFILL_FIXED_RANGE);
     }
 }
 
--- src/menu/MenuOptions.cpp
+++ src/menu/MenuOptions.cpp
@@ -8,23 +8,23 @@ void Menu::create_file_option() {
     file_item->set_submenu(*file_menu);
 
     auto new_item = Gtk::make_managed<Gtk::MenuItem>("Nouveau");
-    new_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::new_file));
+    new_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::new_file));
     file_menu->append(*new_item);
 
     auto open_item = Gtk::make_managed<Gtk::MenuItem>("Ouvrir");
-    open_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::open_file));
+    open_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::open_file));
     file_menu->append(*open_item);
 
     auto save_item = Gtk::make_managed<Gtk::MenuItem>("Enregistrer");
-    save_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::save_file));
+    save_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::save_file));
     file_menu->append(*save_item);
 
     auto save_as_item = Gtk::make_managed<Gtk::MenuItem>("Enregistrer sous");
-    save_as_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::save_as_file));
+    save_as_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::save_as_file));
     file_menu->append(*save_as_item);
 
     auto close_item = Gtk::make_managed<Gtk::MenuItem>("Fermer");
-    close_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::close_file));
+    close_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::close_file));
     file_menu->append(*close_item);
 
     _menu_bar.append(*file_item);
@@ -36,7 +36,7 @@ void Menu::create_display_option() {
     display_item->set_submenu(*display_menu);
 
     auto fullscreen_item = Gtk::make_managed<Gtk::MenuItem>("Plein écran");
-    fullscreen_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::fullscreen));
+    fullscreen_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::fullscreen));
     display_menu->append(*fullscreen_item);
 
     _menu_bar.append(*display_item);
@@ -48,7 +48,7 @@ void Menu::create_image_option() {
     image_item->set_submenu(*image_menu);
 
     auto resize_item = Gtk::make_managed<Gtk::MenuItem>("Redimensionner");
-    resize_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::resize_image));
+    resize_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::resize_image));
     image_menu->append(*resize_item);
 
     _menu_bar.append(*image_item);
@@ -60,7 +60,7 @@ void Menu::create_effect_option() {
     effect_item->set_submenu(*effect_menu);
 
     auto blur_item = Gtk::make_managed<Gtk::MenuItem>("Flou");
-    blur_item->signal_activate().connect(sigc::mem_fun(_window, &Engine::blur_effect));
+    blur_item->signal_activate().connect(sigc::mem_fun(_engine, &Engine::blur_effect));
     effect_menu->append(*blur_item);
 
     _menu_bar.append(*effect_item);
@@ -69,14 +69,36 @@ void Menu::create_effect_option() {
 void Menu::create_sub_menu() {
     auto label            = Gtk::make_managed<Gtk::Label>("Taille du pinceau:");
     auto brush_size_scale = Gtk::make_managed<Gtk::Scale>();
+    auto horizontal_box   = Gtk::make_managed<Gtk::Box>(Gtk::ORIENTATION_HORIZONTAL);
 
-    _sub_menu.set_orientation(Gtk::ORIENTATION_VERTICAL);
     brush_size_scale->set_range(1.0, 400.0);
     brush_size_scale->set_value(_tool_size);
     brush_size_scale->signal_value_changed().connect([this, brush_size_scale]() {
         _tool_size = static_cast<int>(brush_size_scale->get_value());
     });
+
+    _tool_type_combo = Gtk::make_managed<Gtk::ComboBoxText>();
+    horizontal_box->pack_start(*_tool_type_combo, Gtk::PACK_SHRINK);
+    horizontal_box->pack_start(*brush_size_scale, Gtk::PACK_EXPAND_WIDGET);
+    _sub_menu.set_orientation(Gtk::ORIENTATION_VERTICAL);
     _sub_menu.pack_start(*label, Gtk::PACK_SHRINK);
-    _sub_menu.pack_start(*brush_size_scale, Gtk::PACK_SHRINK);
+    _sub_menu.pack_start(*horizontal_box, Gtk::PACK_SHRINK);
 }
+
+void Menu::update_tool_types(std::vector<std::string> tool_types) {
+    _tool_type = 0;
+    _tool_type_combo->remove_all();
+    if (!tool_types.empty()) {
+        for (const auto& option : tool_types) {
+            _tool_type_combo->append(option);
+        }
+        _tool_type_combo->set_active(_tool_type);
+        _tool_type_combo->set_sensitive(true);
+        _tool_type_combo->signal_changed().connect(
+            [this]() { _tool_type = _tool_type_combo->get_active_row_number(); });
+    } else {
+        _tool_type_combo->set_sensitive(false);
+    }
+}
+
 } // namespace vipe
