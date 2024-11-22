d8eb18d0eb0e32d3c3794b3b44310eaf32cc787d
Quentin Duval <quentin1.duval@epitech.eu>
2024-11-22 17:03:30 +0100

    [FIX] new file saving

--- src/menu/FileOptions.cpp
+++ src/menu/FileOptions.cpp
@@ -61,24 +61,45 @@ void Engine::save_as_file() {
     dialog.set_transient_for(*this);
     dialog.add_button("_Annuler", Gtk::RESPONSE_CANCEL);
     dialog.add_button("_Enregistrer", Gtk::RESPONSE_OK);
-
-    auto filter_image = Gtk::FileFilter::create();
-    filter_image->set_name("Images");
-    filter_image->add_mime_type("image/jpeg");
-    filter_image->add_mime_type("image/png");
-    dialog.add_filter(filter_image);
+    Gtk::Box          format_box(Gtk::ORIENTATION_HORIZONTAL);
+    Gtk::Label        format_label("Format :");
+    Gtk::ComboBoxText format_selector;
+    format_selector.append("PNG");
+    format_selector.append("JPEG");
+    format_selector.set_active(0);
+    format_box.pack_start(format_label, Gtk::PACK_SHRINK);
+    format_box.pack_start(format_selector, Gtk::PACK_EXPAND_WIDGET);
+    format_box.show_all();
+    dialog.set_extra_widget(format_box);
 
     int result = dialog.run();
 
     if (result == Gtk::RESPONSE_OK) {
         std::string filename = dialog.get_filename();
-
+        if (filename.empty()) {
+            Gtk::MessageDialog error_dialog("Erreur : Le nom du fichier est vide.", false,
+                                            Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
+            error_dialog.run();
+            return;
+        }
+        std::string format = format_selector.get_active_text();
+        if (format == "PNG" && filename.find(".png") == std::string::npos) {
+            filename += ".png";
+        } else if (format == "JPEG" && filename.find(".jpg") == std::string::npos &&
+                   filename.find(".jpeg") == std::string::npos) {
+            filename += ".jpg";
+        }
         _canva->set_filename(filename);
-        cv::imwrite(_canva->get_filename(), image);
+        if (format == "PNG") {
+            cv::imwrite(_canva->get_filename(), image, {cv::IMWRITE_PNG_COMPRESSION, 3});
+        } else if (format == "JPEG") {
+            cv::imwrite(_canva->get_filename(), image, {cv::IMWRITE_JPEG_QUALITY, 90});
+        }
     }
 }
 
 void Engine::close_file() {
+    return;
     _canvas.pop_back();
     _canva = std::make_shared<Canva>(_canvas.back());
     _drawing_area.queue_draw();
